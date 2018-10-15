a8ed9d871d61da51c9800b4bac2aeb1079c2fb6f
MeganG22 <megangrantmusic@gmail.com>
2018-10-15 18:33:37 +0100

    FirstUpdate
    Focus on importing models/animations, implementing animations & movement scripts.

---
+++ GameTools2Project/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator m_animator;
+    private float m_dodgeX;
+    private float m_walkY;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initialise Animator
+        m_animator = GetComponent<Animator>();
+
+    }
+
+    public void Move(float dodgeX, float walkY)
+    {
+        m_animator.SetFloat("DodgeX", dodgeX);
+        m_animator.SetFloat("WalkY", walkY);
+    }
+
+ }
+
+
---
+++ GameTools2Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private float m_dodgeX;
+    private float m_walkY;
+
+    private Player m_player;
+
+    private void Start()
+    {
+        m_player = GetComponent<Player>();
+    }
+    void FixedUpdate() //physics seconds
+    {
+        //Get Inputs
+        m_dodgeX = Input.GetAxis("Horizontal");
+        m_walkY = Input.GetAxis("Vertical");
+
+        m_player.Move(m_dodgeX, m_walkY);
+    }
+}
+
